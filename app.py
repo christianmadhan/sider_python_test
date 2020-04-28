c897cdb9870256ab2c46292eb5f0c3c6a3dd62e8
KongChristian <christianmadhan@gmail.com>
2020-04-28 16:09:05 +0200

    simple flask application

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-print('hello world')
-print('test 1')
-print('test 2')
-print('test 3')
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run()
